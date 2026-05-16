10843cb050fa9437b50eff569977f97657daec00
Manoj <dhanekulamanoj2002@gmail.com>
2026-05-16 22:53:50 +0530

    Added For Log

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Git Project With Java After First");
+        System.out.println("Git Project With Java After Second");
     }
 }
